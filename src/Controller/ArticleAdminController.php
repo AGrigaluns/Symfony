3f378f86dc10839a5ffec0c6dc4b7885ab8609f0
Alvis Grigaluns <alvis.grigaluns@crealoz.fr>
2020-01-30 13:56:13 +0200

    Role Hierarchy

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @IsGranted("ROLE_ADMIN")
+ * @IsGranted("ROLE_ADMIN_ARTICLE")
  */
 class ArticleAdminController extends AbstractController
 {
