b076a1a90d7d7c19e1857287903ecad97b13abea
Alvis Grigaluns <alvis.grigaluns@crealoz.fr>
2020-02-04 13:02:11 +0200

    under data

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -25,20 +25,33 @@ class ArticleFormType extends AbstractType
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $article = $options['data'] ?? null;
+        $isEdit = $article && $article->getId();
+
         $builder
             ->add('title', TextType::class, [
                 'help' => 'Choose spacy title!',
             ])
-            ->add('content')
-            ->add('publishedAt')
-            ->add('author', UserSelectTextType::class)
+            ->add('content', null, [
+                'rows' => 15
+            ])
+            ->add('author', UserSelectTextType::class, [
+                'disabled' => $isEdit
+            ])
         ;
+
+        if ($options['include_published_at']) {
+            $builder->add('publishedAt', null, [
+                'widget' => 'single_text'
+            ]);
+        }
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-           'data_class' => Article::class
+           'data_class' => Article::class,
+            'include_published_at' => false,
         ]);
     }
 
