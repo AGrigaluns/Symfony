be6dad45c76bf0a10f8392e98633663a2ec5312c
Alvis Grigaluns <alvis.grigaluns@crealoz.fr>
2020-02-04 15:30:18 +0200

    set data

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace App\Form;
+
 use App\Entity\Article;
 use App\Entity\User;
 use App\Repository\ArticleRepository;
@@ -9,22 +11,26 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+
 class ArticleFormType extends AbstractType
 {
     private $userRepository;
+
     public function __construct(UserRepository $userRepository)
     {
         $this->userRepository = $userRepository;
     }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         /** @var Article|null $article */
         $article = $options['data'] ?? null;
         $isEdit = $article && $article->getId();
-        $location = $article ? $article->getLocation() : null;
+
         $builder
             ->add('title', TextType::class, [
                 'help' => 'Choose something catchy!'
@@ -45,22 +51,32 @@ class ArticleFormType extends AbstractType
                 'required' => false,
             ])
         ;
-        if ($location) {
-            $builder->add('specificLocationName', ChoiceType::class, [
-                'placeholder' => 'Where exactly?',
-                'choices' => $this->getLocationNameChoices($location),
-                'required' => false,
-            ]);
-        }
+
         if ($options['include_published_at']) {
             $builder->add('publishedAt', null, [
                 'widget' => 'single_text',
             ]);
         }
 
+        $builder->addEventListener(
+            FormEvents::PRE_SET_DATA,
+            function (FormEvent $event) {
+                /** @var Article|null $data */
+                $data = $event->getData();
+                if (!$data) {
+                    return;
+                }
+
+                $this->setupSpecificLocationNameField(
+                    $event->getForm(),
+                    $data->getLocation()
+                );
+            }
+        );
+
         $builder->get('location')->addEventListener(
             FormEvents::POST_SUBMIT,
-            function (FormEvent $event) {
+            function(FormEvent $event) {
                 $form = $event->getForm();
                 $this->setupSpecificLocationNameField(
                     $form->getParent(),
@@ -68,42 +84,6 @@ class ArticleFormType extends AbstractType
                 );
             }
         );
-
-    }
-    public function configureOptions(OptionsResolver $resolver)
-    {
-        $resolver->setDefaults([
-            'data_class' => Article::class,
-            'include_published_at' => false,
-        ]);
-    }
-    private function getLocationNameChoices(string $location)
-    {
-        $planets = [
-            'Mercury',
-            'Venus',
-            'Earth',
-            'Mars',
-            'Jupiter',
-            'Saturn',
-            'Uranus',
-            'Neptune',
-        ];
-        $stars = [
-            'Polaris',
-            'Sirius',
-            'Alpha Centauari A',
-            'Alpha Centauari B',
-            'Betelgeuse',
-            'Rigel',
-            'Other'
-        ];
-        $locationNameChoices = [
-            'solar_system' => array_combine($planets, $planets),
-            'star' => array_combine($stars, $stars),
-            'interstellar_space' => null,
-        ];
-        return $locationNameChoices[$location];
     }
 
     private function setupSpecificLocationNameField(FormInterface $form, ?string $location)
@@ -116,7 +96,7 @@ class ArticleFormType extends AbstractType
 
         $choices = $this->getLocationNameChoices($location);
 
-        if (null ===$choices) {
+        if (null === $choices) {
             $form->remove('specificLocationName');
 
             return;
@@ -127,5 +107,45 @@ class ArticleFormType extends AbstractType
             'choices' => $choices,
             'required' => false,
         ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Article::class,
+            'include_published_at' => false,
+        ]);
+    }
+
+    private function getLocationNameChoices(string $location)
+    {
+        $planets = [
+            'Mercury',
+            'Venus',
+            'Earth',
+            'Mars',
+            'Jupiter',
+            'Saturn',
+            'Uranus',
+            'Neptune',
+        ];
+
+        $stars = [
+            'Polaris',
+            'Sirius',
+            'Alpha Centauari A',
+            'Alpha Centauari B',
+            'Betelgeuse',
+            'Rigel',
+            'Other'
+        ];
+
+        $locationNameChoices = [
+            'solar_system' => array_combine($planets, $planets),
+            'star' => array_combine($stars, $stars),
+            'interstellar_space' => null,
+        ];
+
+        return $locationNameChoices[$location];
     }
 }
