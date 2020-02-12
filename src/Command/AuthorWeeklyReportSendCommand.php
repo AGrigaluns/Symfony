33fde553b576fa17b92ce5ddd6cd9ae94f32f1c5
Alvis Grigaluns <alvis.grigaluns@crealoz.fr>
2020-02-12 15:01:24 +0200

    attach pdf to mail

--- src/Command/AuthorWeeklyReportSendCommand.php
+++ src/Command/AuthorWeeklyReportSendCommand.php
@@ -4,6 +4,7 @@ namespace App\Command;
 
 use App\Repository\ArticleRepository;
 use App\Repository\UserRepository;
+use Knp\Snappy\Pdf;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -13,61 +14,62 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\NamedAddress;
+use Twig\Environment;
 
 class AuthorWeeklyReportSendCommand extends Command
 {
     protected static $defaultName = 'app:author-weekly-report:send';
-
     private $userRepository;
     private $articleRepository;
     private $mailer;
-
-    public function __construct(UserRepository $userRepository, ArticleRepository $articleRepository, MailerInterface $mailer)
+    private $twig;
+    private $pdf;
+    public function __construct(UserRepository $userRepository, ArticleRepository $articleRepository, MailerInterface $mailer, Environment $twig, Pdf $pdf)
     {
         parent::__construct(null);
         $this->userRepository = $userRepository;
         $this->articleRepository = $articleRepository;
         $this->mailer = $mailer;
+        $this->twig = $twig;
+        $this->pdf = $pdf;
     }
-
     protected function configure()
     {
         $this
             ->setDescription('Send weekly reports to authors')
         ;
     }
-
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $io = new SymfonyStyle($input, $output);
-
         $authors = $this->userRepository
             ->findAllSubscribedToNewsletter();
         $io->progressStart(count($authors));
         foreach ($authors as $author) {
             $io->progressAdvance();
-
             $articles = $this->articleRepository
                 ->findAllPublishedLastWeekByAuthor($author);
-            // skip authors who do not have published articles for the last week
-            if (count($articles) ===0) {
+            // Skip authors who do not have published articles for the last week
+            if (count($articles) === 0) {
                 continue;
             }
-
+            $html = $this->twig->render('email/author-weekly-report-pdf.html.twig', [
+                'articles' => $articles,
+            ]);
+            $pdf = $this->pdf->getOutputFromHtml($html);
             $email = (new TemplatedEmail())
-                ->from(new NamedAddress('alienmailer@example.com', 'The Space Bar'))
+                ->from(new NamedAddress('alienmailcarrier@example.com', 'The Space Bar'))
                 ->to(new NamedAddress($author->getEmail(), $author->getFirstName()))
-                ->subject('Your weekly report on The Space Bar!')
+                ->subject('Your weekly report on the Space Bar!')
                 ->htmlTemplate('email/author-weekly-report.html.twig')
                 ->context([
-                   'author' => $author,
-                   'articles' => $articles
-                ]);
-
+                    'author' => $author,
+                    'articles' => $articles,
+                ])
+                ->attach($pdf, sprintf('weekly-report-%s.pdf', date('Y-m-d')));
             $this->mailer->send($email);
         }
         $io->progressFinish();
-
         $io->success('Weekly reports were sent to authors!');
     }
 }
