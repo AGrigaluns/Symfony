f9ff2e158eff648492ba55e38bf9479321e61fd8
Alvis Grigaluns <alvis.grigaluns@crealoz.fr>
2020-02-17 11:13:43 +0200

    file upload

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -1,18 +1,19 @@
 <?php
-namespace App\Controller;
 
+namespace App\Controller;
 
 use App\Entity\Article;
 use App\Form\ArticleFormType;
-use Gedmo\Sluggable\Util\Urlizer;
 use App\Repository\ArticleRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Gedmo\Sluggable\Util\Urlizer;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 class ArticleAdminController extends BaseController
 {
     /**
@@ -22,19 +23,25 @@ class ArticleAdminController extends BaseController
     public function new(EntityManagerInterface $em, Request $request)
     {
         $form = $this->createForm(ArticleFormType::class);
+
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             /** @var Article $article */
             $article = $form->getData();
+
             $em->persist($article);
             $em->flush();
+
             $this->addFlash('success', 'Article Created! Knowledge is power!');
+
             return $this->redirectToRoute('admin_article_list');
         }
+
         return $this->render('article_admin/new.html.twig', [
             'articleForm' => $form->createView()
         ]);
     }
+
     /**
      * @Route("/admin/article/{id}/edit", name="admin_article_edit")
      * @IsGranted("MANAGE", subject="article")
@@ -44,38 +51,40 @@ class ArticleAdminController extends BaseController
         $form = $this->createForm(ArticleFormType::class, $article, [
             'include_published_at' => true
         ]);
+
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
+            /** @var UploadedFile $uploadedFile */
+            $uploadedFile = $form['imageFile']->getData();
+
+            if ($uploadedFile) {
+                $destination = $this->getParameter('kernel.project_dir') . '/public/uploads/article_image';
+
+                $originalFilename = pathinfo($uploadedFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $newFilename = Urlizer::urlize($originalFilename) . '-' . uniqid() . '.' . $uploadedFile->guessExtension();
+
+                $uploadedFile->move(
+                    $destination,
+                    $newFilename
+                );
+                $article->setImageFilename($newFilename);
+            }
+
             $em->persist($article);
             $em->flush();
+
             $this->addFlash('success', 'Article Updated! Inaccuracies squashed!');
+
             return $this->redirectToRoute('admin_article_edit', [
                 'id' => $article->getId(),
             ]);
         }
+
         return $this->render('article_admin/edit.html.twig', [
             'articleForm' => $form->createView()
         ]);
     }
-    /**
-     * @Route("/admin/upload/test", name="upload_test")
-     */
-    public function temporaryUploadAction(Request $request)
-    {
-        /** @var UploadedFile $uploadedFile */
-        $uploadedFile = $request->files->get('image');
-        $destination = $this->getParameter('kernel.project_dir').'/public/uploads';
 
-
-        $originalFilename = pathinfo($uploadedFile->getClientOriginalName(), PATHINFO_FILENAME);
-        // uniqid() is for unique file name, when user uploads file with the same name it will be with different id
-        $newFilename = Urlizer::urlize($originalFilename).'-'.uniqid().'-'.$uploadedFile->guessExtension();
-
-        dd($uploadedFile->move(
-            $destination,
-            $newFilename
-        ));
-    }
     /**
      * @Route("/admin/article/location-select", name="admin_article_location_select")
      * @IsGranted("ROLE_USER")
@@ -86,17 +95,21 @@ class ArticleAdminController extends BaseController
         if (!$this->isGranted('ROLE_ADMIN_ARTICLE') && $this->getUser()->getArticles()->isEmpty()) {
             throw $this->createAccessDeniedException();
         }
+
         $article = new Article();
         $article->setLocation($request->query->get('location'));
         $form = $this->createForm(ArticleFormType::class, $article);
+
         // no field? Return an empty response
         if (!$form->has('specificLocationName')) {
             return new Response(null, 204);
         }
+
         return $this->render('article_admin/_specific_location_name.html.twig', [
             'articleForm' => $form->createView(),
         ]);
     }
+
     /**
      * @Route("/admin/article", name="admin_article_list")
      * @IsGranted("ROLE_ADMIN_ARTICLE")
@@ -104,6 +117,7 @@ class ArticleAdminController extends BaseController
     public function list(ArticleRepository $articleRepo)
     {
         $articles = $articleRepo->findAll();
+
         return $this->render('article_admin/list.html.twig', [
             'articles' => $articles,
         ]);
