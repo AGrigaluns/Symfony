cad0b8aa51e98b8d8564c8d3dad2c3c737687ebe
Alvis Grigaluns <alvis.grigaluns@crealoz.fr>
2020-02-17 10:16:02 +0200

    unique filenames

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -1,9 +1,10 @@
 <?php
-
 namespace App\Controller;
+
 
 use App\Entity\Article;
 use App\Form\ArticleFormType;
+use Gedmo\Sluggable\Util\Urlizer;
 use App\Repository\ArticleRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
@@ -12,7 +13,6 @@ use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
 class ArticleAdminController extends BaseController
 {
     /**
@@ -22,25 +22,19 @@ class ArticleAdminController extends BaseController
     public function new(EntityManagerInterface $em, Request $request)
     {
         $form = $this->createForm(ArticleFormType::class);
-
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             /** @var Article $article */
             $article = $form->getData();
-
             $em->persist($article);
             $em->flush();
-
             $this->addFlash('success', 'Article Created! Knowledge is power!');
-
             return $this->redirectToRoute('admin_article_list');
         }
-
         return $this->render('article_admin/new.html.twig', [
             'articleForm' => $form->createView()
         ]);
     }
-
     /**
      * @Route("/admin/article/{id}/edit", name="admin_article_edit")
      * @IsGranted("MANAGE", subject="article")
@@ -50,36 +44,36 @@ class ArticleAdminController extends BaseController
         $form = $this->createForm(ArticleFormType::class, $article, [
             'include_published_at' => true
         ]);
-
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $em->persist($article);
             $em->flush();
-
             $this->addFlash('success', 'Article Updated! Inaccuracies squashed!');
-
             return $this->redirectToRoute('admin_article_edit', [
                 'id' => $article->getId(),
             ]);
         }
-
         return $this->render('article_admin/edit.html.twig', [
             'articleForm' => $form->createView()
         ]);
     }
-
     /**
      * @Route("/admin/upload/test", name="upload_test")
      */
     public function temporaryUploadAction(Request $request)
     {
         /** @var UploadedFile $uploadedFile */
-        $uploadedFile = $request->files->get('images');
+        $uploadedFile = $request->files->get('image');
         $destination = $this->getParameter('kernel.project_dir').'/public/uploads';
 
-        dd($uploadedFile->move($destination));
-    }
+        $originalFilename = pathinfo($uploadedFile->getClientOriginalName(), PATHINFO_FILENAME);
+        $newFilename = Urlizer::urlize($originalFilename).'-'.uniqid().'-'.$uploadedFile->guessExtension();
 
+        dd($uploadedFile->move(
+            $destination,
+            $newFilename
+        ));
+    }
     /**
      * @Route("/admin/article/location-select", name="admin_article_location_select")
      * @IsGranted("ROLE_USER")
@@ -90,21 +84,17 @@ class ArticleAdminController extends BaseController
         if (!$this->isGranted('ROLE_ADMIN_ARTICLE') && $this->getUser()->getArticles()->isEmpty()) {
             throw $this->createAccessDeniedException();
         }
-
         $article = new Article();
         $article->setLocation($request->query->get('location'));
         $form = $this->createForm(ArticleFormType::class, $article);
-
         // no field? Return an empty response
         if (!$form->has('specificLocationName')) {
             return new Response(null, 204);
         }
-
         return $this->render('article_admin/_specific_location_name.html.twig', [
             'articleForm' => $form->createView(),
         ]);
     }
-
     /**
      * @Route("/admin/article", name="admin_article_list")
      * @IsGranted("ROLE_ADMIN_ARTICLE")
@@ -112,7 +102,6 @@ class ArticleAdminController extends BaseController
     public function list(ArticleRepository $articleRepo)
     {
         $articles = $articleRepo->findAll();
-
         return $this->render('article_admin/list.html.twig', [
             'articles' => $articles,
         ]);
