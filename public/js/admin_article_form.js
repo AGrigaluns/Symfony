1728505c1ffa77d358db95824d56f3d8df02566f
Alvis Grigaluns <alvis.grigaluns@crealoz.fr>
2020-02-18 18:14:41 +0200

    deleting files

--- public/js/admin_article_form.js
+++ public/js/admin_article_form.js
@@ -1,12 +1,12 @@
 Dropzone.autoDiscover = false;
 
 $(document).ready(function() {
-    let referenceList = new ReferenceList($('.js-reference-list'));
+    var referenceList = new ReferenceList($('.js-reference-list'));
 
     initializeDropzone(referenceList);
 
-    let $locationSelect = $('.js-article-form-location');
-    let $specificLocationTarget = $('.js-specific-location-target');
+    var $locationSelect = $('.js-article-form-location');
+    var $specificLocationTarget = $('.js-specific-location-target');
 
     $locationSelect.on('change', function(e) {
         $.ajax({
@@ -32,13 +32,17 @@ $(document).ready(function() {
 });
 
 // todo - use Webpack Encore so ES6 syntax is transpiled to ES5
-
 class ReferenceList
 {
     constructor($element) {
         this.$element = $element;
         this.references = [];
         this.render();
+
+        this.$element.on('click', '.js-reference-delete', (event) => {
+            this.handleReferenceDelete(event);
+        });
+
         $.ajax({
             url: this.$element.data('url')
         }).then(data => {
@@ -52,17 +56,36 @@ class ReferenceList
         this.render();
     }
 
+    handleReferenceDelete(event) {
+        const $li = $(event.currentTarget).closest('.list-group-item');
+        const id = $li.data('id');
+        $li.addClass('disabled');
+
+        $.ajax({
+            url: '/admin/article/references/'+id,
+            method: 'DELETE'
+        }).then(() => {
+            this.references = this.references.filter(reference => {
+                return reference.id !== id;
+            });
+            this.render();
+        });
+    }
+
     render() {
         const itemsHtml = this.references.map(reference => {
             return `
-<li class="list-group-item d-flex justify-content-between align-items-center">
+<li class="list-group-item d-flex justify-content-between align-items-center" data-id="${reference.id}">
     ${reference.originalFilename}
+
     <span>
-        <a href="/admin/article/references/${reference.id}/download"><span class="fa fa-download"></span></a>
+        <a href="/admin/article/references/${reference.id}/download" class="btn btn-link btn-sm"><span class="fa fa-download" style="vertical-align: middle"></span></a>
+        <button class="js-reference-delete btn btn-link btn-sm"><span class="fa fa-trash"></span></button>
     </span>
 </li>
 `
         });
+
         this.$element.html(itemsHtml.join(''));
     }
 }
@@ -71,14 +94,15 @@ class ReferenceList
  * @param {ReferenceList} referenceList
  */
 function initializeDropzone(referenceList) {
-    let formElement = document.querySelector('.js-reference-dropzone');
+    var formElement = document.querySelector('.js-reference-dropzone');
     if (!formElement) {
         return;
     }
-    let dropzone = new Dropzone(formElement, {
+
+    var dropzone = new Dropzone(formElement, {
         paramName: 'reference',
         init: function() {
-            this.on('success', function (file, data) {
+            this.on('success', function(file, data) {
                 referenceList.addReference(data);
             });
 
