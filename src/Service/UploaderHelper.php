a91829eae9ef9724df9df9afbca096408e958cfe
Alvis Grigaluns <alvis.grigaluns@crealoz.fr>
2020-02-18 12:02:39 +0200

    storing private files

--- src/Service/UploaderHelper.php
+++ src/Service/UploaderHelper.php
@@ -15,15 +15,18 @@ use Symfony\Component\HttpFoundation\File\UploadedFile;
 class UploaderHelper
 {
     const ARTICLE_IMAGE = 'article_image';
+    const ARTICLE_REFERENCE = 'article_reference';
 
     private $filesystem;
+    private $privateFilesystem;
     private $requestStackContext;
     private $logger;
     private $publicAssetBaseUrl;
 
-    public function __construct(FilesystemInterface $publicUploadFilesystem, RequestStackContext $requestStackContext, LoggerInterface $logger, string $uploadedAssetsBaseUrl)
+    public function __construct(FilesystemInterface $publicUploadFilesystem, FilesystemInterface $privateUploadFilesystem, RequestStackContext $requestStackContext, LoggerInterface $logger, string $uploadedAssetsBaseUrl)
     {
         $this->filesystem = $publicUploadFilesystem;
+        $this->privateFilesystem = $privateUploadFilesystem;
         $this->requestStackContext = $requestStackContext;
         $this->logger = $logger;
         $this->publicAssetBaseUrl = $uploadedAssetsBaseUrl;
@@ -32,27 +35,7 @@ class UploaderHelper
     public function uploadArticleImage(File $file, ?string $existingFilename): string
     {
 
-        if ($file instanceof UploadedFile) {
-            $originalFilename = $file->getClientOriginalName();
-        } else {
-            $originalFilename = $file->getFilename();
-        }
-
-        $newFilename = Urlizer::urlize(pathinfo($originalFilename, PATHINFO_FILENAME)) . '-' . uniqid() . '.' . $file->guessExtension();
-
-        $stream = fopen($file->getPathname(), 'r');
-        $result = $this->filesystem->writeStream(
-            self::ARTICLE_IMAGE.'/'.$newFilename,
-            $stream
-        );
-
-        if ($result === false) {
-            throw new \Exception(sprintf('Could not write uploaded file "%s"', $newFilename));
-        }
-
-        if (is_resource($stream)) {
-            fclose($stream);
-        }
+        $newFilename = $this->uploadFile($file, self::ARTICLE_IMAGE, true);
 
         if ($existingFilename) {
             try {
@@ -72,12 +55,42 @@ class UploaderHelper
 
     public function uploadArticleReference(File $file): string
     {
-        dd($file);
+        return $this->uploadFile($file, self::ARTICLE_REFERENCE, false);
     }
 
     public function getPublicPath(string $path): string
     {
         return $this->requestStackContext
             ->getBasePath().$this->publicAssetBaseUrl.'/'.$path;
+    }
+
+    private function uploadFile (File $file, string $directory, bool $isPublic): string
+    {
+        if ($file instanceof UploadedFile) {
+            $originalFilename = $file->getClientOriginalName();
+        } else {
+            $originalFilename = $file->getFilename();
+        }
+
+        $newFilename = Urlizer::urlize(pathinfo($originalFilename, PATHINFO_FILENAME)) . '-' . uniqid() . '.' . $file->guessExtension();
+
+        $filesystem = $isPublic ? $this->filesystem : $this->privateFilesystem;
+
+
+        $stream = fopen($file->getPathname(), 'r');
+        $result = $filesystem->writeStream(
+            $directory.'/'.$newFilename,
+            $stream
+        );
+
+        if ($result === false) {
+            throw new \Exception(sprintf('Could not write uploaded file "%s"', $newFilename));
+        }
+
+        if (is_resource($stream)) {
+            fclose($stream);
+        }
+
+        return $newFilename;
     }
 }
